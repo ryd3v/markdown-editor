3a9dace2809991fa8ab1fcc82aada561c9f0a713
ryancollins-dev <email@ryd3v.rocks>
2022-10-14 05:57:14 -0400

    update menu items

--- menu.js
+++ menu.js
@@ -12,26 +12,30 @@ const template = [
       },
     ],
   },
-  {
-    label: 'Debugging',
-    submenu: [
-      {
-        label: 'Dev Tools',
-        role: 'toggleDevTools',
-      },
-      { type: 'separator' },
-      {
-        role: 'reload',
-        accelerator: 'Alt+R',
-      },
-    ],
-  },
 ];
 
 if (process.platform === 'darwin') {
   template.unshift({
     label: app.getName(),
     submenu: [{ role: 'about' }, { type: 'separator' }, { role: 'quit' }],
+  });
+}
+
+if (process.env.DEBUG) {
+  template.push({
+    label: 'Debugging',
+    submenu: [
+      {
+        label: 'Dev Tools',
+        role: 'toggleDevTools',
+      },
+
+      { type: 'separator' },
+      {
+        role: 'reload',
+        accelerator: 'Alt+R',
+      },
+    ],
   });
 }
 
